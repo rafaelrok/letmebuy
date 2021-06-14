7d580176ae8eac6263ff8e026332a6d8534ea6b7
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-06-13 23:24:33 -0300

    test EmptyResultDataAccessException

--- backend/src/test/java/com/rafaelvieira/dscatalog/repository/ProductRepositoryTests.java
+++ backend/src/test/java/com/rafaelvieira/dscatalog/repository/ProductRepositoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -18,9 +19,20 @@ public class ProductRepositoryTests {
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
 
+        //Teste de deleta basico quando id existe
         long exintingId = 1L;
         repository.deleteById(exintingId);
         Optional<Product> result = repository.findById(exintingId);
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist(){
+
+        //Teste com produto que existe na base
+        long nonexistintingId = 20L;
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+            repository.deleteById(nonexistintingId);
+        });
     }
 }
