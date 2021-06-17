e650b21c8b23108937a95f83151004369b5724d7
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-06-17 01:22:32 -0300

    ProductServicesTests(DataBaseException)

--- backend/src/test/java/com/rafaelvieira/dscatalog/services/ProductServicesTests.java
+++ backend/src/test/java/com/rafaelvieira/dscatalog/services/ProductServicesTests.java
@@ -1,6 +1,7 @@
 package com.rafaelvieira.dscatalog.services;
 
 import com.rafaelvieira.dscatalog.repository.ProductRepository;
+import com.rafaelvieira.dscatalog.services.handlers.DataBaseException;
 import com.rafaelvieira.dscatalog.services.handlers.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -23,16 +25,30 @@ public class ProductServicesTests {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() throws Exception{
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
 
         //Teste com ID existente na base (Remover o mockito o sistema importa staticamente os metodos)
         Mockito.doNothing().when(repository).deleteById(existingId);
         //Teste quando não existe o ID na base lança uma exception
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+        //Verifica a integridade se tem uma categoria associado ao produto que esta tentando excluir(porem a categoria
+        //Esta idependente uma classe distinta)
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+    }
+
+    @Test
+    public void deleteShouldThrowDataBaseExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(DataBaseException.class, () -> {
+            service.delete(dependentId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
     }
 
     @Test
