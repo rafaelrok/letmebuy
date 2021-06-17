b523854967ded146e73d559ebb4ff232726ce44e
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-06-17 01:14:59 -0300

    ProductServicesTests(ResourceNotFoundException)

--- backend/src/test/java/com/rafaelvieira/dscatalog/services/ProductServicesTests.java
+++ backend/src/test/java/com/rafaelvieira/dscatalog/services/ProductServicesTests.java
@@ -1,6 +1,7 @@
 package com.rafaelvieira.dscatalog.services;
 
 import com.rafaelvieira.dscatalog.repository.ProductRepository;
+import com.rafaelvieira.dscatalog.services.handlers.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,10 +29,19 @@ public class ProductServicesTests {
         existingId = 1L;
         nonExistingId = 1000L;
 
-        //Teste com ID existente na base
+        //Teste com ID existente na base (Remover o mockito o sistema importa staticamente os metodos)
         Mockito.doNothing().when(repository).deleteById(existingId);
         //Teste quando não existe o ID na base lança uma exception
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
     }
 
     @Test
