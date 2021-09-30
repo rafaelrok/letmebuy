dd7b8fa8cca3164e1a9e30dc542bc928937edb6a
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-09-30 01:25:30 -0300

    Catalog listing

--- letmebuy-frontend/src/pages/Catalog/index.tsx
+++ letmebuy-frontend/src/pages/Catalog/index.tsx
@@ -3,28 +3,35 @@ import ProductCard from "components/ProductCard";
 import { Link } from "react-router-dom";
 import { Product } from "types/product";
 
+import { useEffect, useState } from 'react';
+import { SpringPage } from "types/vendor/spring";
+import { AxiosParams } from "types/vendor/axios";
+import { BASE_URL } from "util/requests";
+import axios from "axios";
+
 import './stykes.css';
+
+
 
 const Catalog = () => {
 
-    const product: Product = {
-        "id": 2,
-        "name": "Smart TV",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "price": 2190.0,
-        "imgUrl": "https://i.ibb.co/QKjGx1y/itx-gamer-pc-monstro-i9-99000k-2080-super-work-1600x1600fill-ffffff-640x640-fill-ffffff.png",
-        "date": "2020-07-14T10:00:00Z",
-        "categories": [
-            {
-                "id": 1,
-                "name": "Livros"
+    const [page, setPage] = useState<SpringPage<Product>>();
+
+    useEffect(() => {
+        const params: AxiosParams = {
+            method: 'GET',
+            url: `${BASE_URL}/products`,
+            params: {
+                page: 0,
+                size: 12,
             },
-            {
-                "id": 3,
-                "name": "Computadores"
-            }
-        ]
-    }
+        }
+
+        axios(params)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, []);
 
     return (
         <div className="container my-4 catalog-container">
@@ -32,31 +39,15 @@ const Catalog = () => {
                 <h1>Catalo de Produtos</h1>
             </div>
             <div className="row">
-                <div className="col-sm-6 col-lg-4 col-xl-3">
-                    <Link to ="/products/1">
-                        <ProductCard product={product} />
-                    </Link>
-                </div>
-                <div className="col-sm-6 col-lg-4 col-xl-3">
-                    <Link to="/products/2">
-                        <ProductCard product={product} />
-                    </Link>
-                </div>
-                <div className="col-sm-6 col-lg-4 col-xl-3">
-                    <Link to="/products/3">
-                        <ProductCard product={product} />
-                    </Link>
-                </div>
-                <div className="col-sm-6 col-lg-4 col-xl-3">
-                    <Link to="/products/4">
-                        <ProductCard product={product} />
-                    </Link>
-                </div>
-                <div className="col-sm-6 col-lg-4 col-xl-3">
-                    <Link to="/products/5">
-                        <ProductCard product={product} />
-                    </Link>
-                </div>
+
+                {page?.content.map(product => (
+                    <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+                        <Link to="/products/1">
+                            <ProductCard product={product} />
+                        </Link>
+                    </div>
+                ))}
+
             </div>
             <div className="row">
                 <Pagination />
