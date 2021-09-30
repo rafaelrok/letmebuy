3f9c331cc358820c63d782e5e0c3a6c100d0693c
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-09-30 00:54:55 -0300

    useState, UseEffect, useParams

--- letmebuy-frontend/src/pages/ProductDetails/index.tsx
+++ letmebuy-frontend/src/pages/ProductDetails/index.tsx
@@ -1,32 +1,56 @@
 import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
+import axios from 'axios';
+import { BASE_URL } from 'util/requests';
+import { useEffect, useState } from 'react';
+
+
+type UrlParams = {
+    productId: string;
+}
 
 const ProductDetails = () => {
+
+    const { productId } = useParams<UrlParams>();
+
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/products/${productId}`)
+            .then(response => {
+                setProduct(response.data)
+            });
+    }, [productId]);
+
+
     return (
         <div className="product-detail-container">
             <div className="base-card product-detail-card">
                 <Link to="/products">
-                <div className="goback-container">
-                    <ArrowIcon />
-                    <h2>VOLTAR</h2>
-                </div>
+                    <div className="goback-container">
+                        <ArrowIcon />
+                        <h2>VOLTAR</h2>
+                    </div>
                 </Link>
                 <div className="row">
                     <div className="col-xl-6">
                         <div className="img-container">
-                            <img src="https://i.ibb.co/QKjGx1y/itx-gamer-pc-monstro-i9-99000k-2080-super-work-1600x1600fill-ffffff-640x640-fill-ffffff.png" alt="Nome do Produto"></img>
+                            <img src={product?.imgUrl}
+                                alt={product?.name}>
+                            </img>
                         </div>
                         <div className="name-price-container">
-                            <h1>Nome do Produto</h1>
-                            <ProductPrice price={2354.6}/>
+                            <h1>{product?.name}</h1>
+                            {product && <ProductPrice price={product?.price} />}
                         </div>
                     </div>
                     <div className="col-xl-6">
                         <div className="description-container">
                             <h2>Descrição do Produto</h2>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, ducimus.</p>
+                            <p>{product?.description}</p>
                         </div>
                     </div>
                 </div>
