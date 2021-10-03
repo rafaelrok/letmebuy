a0b3a6629470435671390afc26a219aec8c604bf
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-10-03 02:01:12 -0300

    Banner Styled

--- letmebuy-frontend/src/pages/Dashboard/Auth/index.tsx
+++ letmebuy-frontend/src/pages/Dashboard/Auth/index.tsx
@@ -2,14 +2,16 @@
 import { Route, Switch } from 'react-router-dom';
 import { ReactComponent as AuthImage } from '../../../assets/images/auth-image.svg';
 
+import './styles.css';
+
 const Auth = () => {
-    
+
 
     return (
         <div className="auth-container">
             <div className="auth-banner-container">
                 <h1>
-                    Divulgue aqui os seu produtos no lemebuy
+                    Divulgue seu produtos no lemebuy
                 </h1>
                 <p>
                     Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.
