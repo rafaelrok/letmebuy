2a2e6e0f674dd6b5aefc76b1398a936d61bfac4b
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-10-05 07:43:44 -0300

    fuñction hasAnyRoles

--- letmebuy-frontend/src/pages/Home/index.tsx
+++ letmebuy-frontend/src/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import './styles.css';
 import ButtonIcon from "components/ButtonIcon";
 import { Link } from 'react-router-dom';
+import { hasAnyRoles } from 'util/requests';
 
 
 const Home = () => {
     return (
         <div className="home-container">
+            <h1>Resultado:  {hasAnyRoles(['ROLE_ADMIN']) ? 'sim' : 'não'}</h1>
             <div className="base-card home-card">
                 <div className="home-content-container">
                     <div>
--- letmebuy-frontend/src/util/requests.ts
+++ letmebuy-frontend/src/util/requests.ts
@@ -119,3 +119,31 @@ export const isAuthenticated = (): boolean => {
     const tokenData = getTokenData();
     return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyRoles = (roles: Role[]): boolean => {
+    if (roles.length === 0) {
+        return true;
+    }
+
+    //Percorre os roles do type(tokenData.authorities) e verifica se contem o role do usuário logado
+    const tokenData = getTokenData();
+
+    // Metodo (some) função de alta ordem, testando o elemento de uma lista
+    if (tokenData !== undefined) {
+        return roles.some(role => tokenData.authorities.includes(role));
+    }
+
+    /*
+    // Metodo alternativo percorrento com for
+    if (tokenData !== undefined) {
+        for (var i = 0; i < roles.length; i++){
+            if (tokenData.authorities.includes(roles[i])) {
+                return true;
+            }
+        }
+    }
+    */
+
+    return false;
+    
+}
