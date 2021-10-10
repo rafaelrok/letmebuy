18be075540fc96fd64f367357002b092439488e6
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-10-10 18:28:11 -0300

    Category Badge

--- letmebuy-frontend/src/pages/Dashboard/Products/List/index.tsx
+++ letmebuy-frontend/src/pages/Dashboard/Products/List/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import ProductCrudCard from "../../../../components/ProductCrudCard";
+import ProductCrudCard from "../ProductCrudCard";
 import './styles.css';
 
 const List = () => {
