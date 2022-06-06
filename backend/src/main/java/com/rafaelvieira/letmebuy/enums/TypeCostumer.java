c18f47be250e397cc00c5f247190af4b3424ef09
rafaelrok <rafaelvieirasantos2006@hotmail.com>
2022-06-06 20:55:04 -0300

    Implementation enums costumer and payment

---
+++ backend/src/main/java/com/rafaelvieira/letmebuy/enums/TypeCostumer.java
@@ -0,0 +1,40 @@
+package com.rafaelvieira.letmebuy.enums;
+
+public enum TypeCostumer {
+
+    PESSOAFISICA(1, "Pessoa Física"),
+    PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+    private int code;
+    private String description;
+
+    //tipo enum o construtor é privado
+    private TypeCostumer(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+    //Somente getters
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    //Statico para se iniciada mesmo que não tenha instanciado
+    public static TypeCostumer toEnum(Integer code) {
+
+        if (code == null) {
+            return null;
+        }
+
+        for (TypeCostumer x : TypeCostumer.values()) {
+            if (code.equals(x.getCode())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Id inválido: " + code);
+    }
+}
---
+++ backend/src/main/java/com/rafaelvieira/letmebuy/enums/TypePayment.java
@@ -0,0 +1,39 @@
+package com.rafaelvieira.letmebuy.enums;
+
+public enum TypePayment {
+
+    PENDENTE(1, "Pendente"),
+    QUITADO(2, "Quitado"),
+    CANCELADO(3, "Cancelado");
+
+    private final int code;
+    private final String description;
+
+    private TypePayment(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public static TypePayment toEnum(Integer code) {
+
+        if (code == null) {
+            return null;
+        }
+
+        for (TypePayment x : TypePayment.values()) {
+            if (code.equals(x.getCode())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Id inválido: " + code);
+    }
+}
